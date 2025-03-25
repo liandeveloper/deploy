9b4d8eca1dfdaa6ac9e8c79f06e4dbcffede2cdd
Lian <158589954+liandeveloper@users.noreply.github.com>
2025-03-25 00:31:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const mobileMenu = document.getElementById('menu-mobile');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+function sendToWhatsApp(id) {
+    const phoneNumber = "53123456789"; // Reemplaza con tu número de WhatsApp
+    const message = `Hola, estoy interesado en el producto con ID: ${id}. ¿Está disponible?`;
+    const encodedMessage = encodeURIComponent(message);
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.location.href = url;
+}
