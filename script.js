eb777162fb71ea7a337866690e9c42fe31b28ee7
Lian <158589954+liandeveloper@users.noreply.github.com>
2025-03-28 21:48:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ mobileMenu.addEventListener('click', () => {
 });
 
 function sendToWhatsApp(id) {
-    const phoneNumber = "53123456789"; // Reemplaza con tu número de WhatsApp
+    const phoneNumber = "5355105070"; // Reemplaza con tu número de WhatsApp
     const message = `Hola, estoy interesado en el producto con ID: ${id}. ¿Está disponible?`;
     const encodedMessage = encodeURIComponent(message);
     const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
